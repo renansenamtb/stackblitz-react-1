23e99a551d7490145bb842f636771afcdea57573
renansenamtb <renansenazzz@gmail.com>
2025-04-03 14:29:53 -0400

    css do bão
    ta pronto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { auth } from './config/firebaseConfig.js';
 import { signInWithEmailAndPassword } from 'firebase/auth';
-import styles from './styles/vigia.module.css'
+import styles from './styles/vigia.module.css'; 
 
 export default function App() {
   const [email, setEmail] = useState('');
@@ -11,34 +11,40 @@ export default function App() {
     evento.preventDefault();
     try {
       await signInWithEmailAndPassword(auth, email, senha);
-      alert('Autenticação bem-sucedida');
+      alert('Logado com sucesso!');
     } catch (error) {
-      console.error('Erro ao autenticar:', error);
-      alert('Erro na autenticação');
+      alert('Erro no processo!');
     }
   };
 
   return (
-    <main>
-      <form onSubmit={autenticarComFirebase}>
-        <label htmlFor="email">E-mail:</label>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          value={email}
-          onChange={(evento) => setEmail(evento.target.value)}
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/en/e/ea/Mike_Ehrmantraut_BCS_S3.png" 
+          alt="idoso do breaking bad"
+          className={styles.imagem}
         />
-        <label htmlFor="password">Senha:</label>
-        <input
-          id="password"
-          name="password"
-          type="password"
-          value={senha}
-          onChange={(evento) => setSenha(evento.target.value)}
-        />
-        <button type="submit">Entrar</button>
-      </form>
-    </main>
+        <form onSubmit={autenticarComFirebase}>
+          <label htmlFor="email">E-mail:</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            value={email}
+            onChange={(evento) => setEmail(evento.target.value)}
+          />
+          <label htmlFor="password">Senha:</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            value={senha}
+            onChange={(evento) => setSenha(evento.target.value)}
+          />
+          <button type="submit">Entrar</button>
+        </form>
+      </main>
+    </div>
   );
 }
